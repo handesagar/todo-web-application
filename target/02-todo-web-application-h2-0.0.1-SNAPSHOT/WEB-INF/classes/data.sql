5d3b9173fede13b4bca182e854058ccd3211a9a4
Sagar Hande <Sagar.Hande@vanderlande.com>
2020-05-13 15:45:06 +0530

    first commit

--- target/02-todo-web-application-h2-0.0.1-SNAPSHOT/WEB-INF/classes/data.sql
+++
@@ -1,6 +0,0 @@
-insert into TODO
-values(10001, 'MicroServices Architecture', false, sysdate(), 'https://youtu.be/cymV-DIkiHI');
-insert into TODO
-values(10002, 'Domain Driven Design', false, sysdate(), 'https://youtu.be/6zPWTIW8s0Q');
-insert into TODO
-values(10003, 'Docker,Kubernetes', false, sysdate(), 'https://youtu.be/REYjjhX9Yns');
